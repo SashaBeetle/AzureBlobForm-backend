da1f8296b7d93edd80647ce3aed1d729a0885314
Олександр Жук <oleksandr.zhuk@lnu.edu.ua>
2024-02-20 22:38:50 +0200

    Add email for POST.

--- AzureBlobForm-backend.WEB/Program.cs
+++ AzureBlobForm-backend.WEB/Program.cs
@@ -1,8 +1,19 @@
 using AzureBlobForm_backend.Core.Interfaces;
 using AzureBlobForm_backend.Models.Repository;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:3000",
+                                              "http://localhost:3000");
+                      });
+});
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -21,6 +32,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
